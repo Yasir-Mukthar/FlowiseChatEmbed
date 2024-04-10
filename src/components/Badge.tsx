88554eaff1002a31bf53c01f97d4ba05dd4701e8
Yasir-Mukthar <yasirmukhtartar@example.com>
2024-04-10 00:06:45 -0700

    Update Badge component to use 'BidFair' instead of 'Flowise'

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -47,14 +47,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'http://localhost:5173/dashboard'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> BidFair</span>
       </a>
     </span>
   );
